451d35859ffa271ff5ce90b966a320cdd3e6f091
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-11 21:34:35 -0300

    rotas criadas

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Login from './pages/login/login';
 import "./styles/global.css";
-import Register from './pages/register/register';
+import Rotas from './rotas';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Register />
+    <Rotas />
 );
---
+++ src/rotas.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login/login";
+import Register from "./pages/register/register";
+import Appointments from "./pages/appointments/appointments";
+
+
+function Rotas() {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Login />}/>
+            <Route path="/register" element={<Register />}/>
+            <Route path="/appointments" element={<Appointments />}/>
+        </Routes>
+    </BrowserRouter>
+    )
+}
+
+export default Rotas;
