62ba9d831d6fabcef5f27a61574d4dc685a4caf9
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-11-13 10:19:38 -0300

    sistema de login e register feito

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,13 +2,50 @@ import "./login.css";
 import logo from "../../assets/logo.png";
 import fundo from "../../assets/fundo.png";
 import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import api from "../../constants/api.js";
 
 function Login() {
 
     const navigate = useNavigate();
 
-    function ExecuteLogin() {
-        navigate("/appointments");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [msg, setMsg] = useState("");
+
+    async function ExecuteLogin() {
+
+        setMsg("");
+
+        try {
+            const response = await api.post("/users/login", {
+                email,
+                password
+            });
+
+            if (response.data) {
+                localStorage.setItem("sessionToken", response.data.token);
+                localStorage.setItem("sessionId", response.data.id_user);
+                localStorage.setItem("sessionEmail", response.data.email);
+                localStorage.setItem("sessionName", response.data.name);
+                
+                navigate("/appointments");
+            } else {
+                setMsg("Erro ao fazer login, tente novamente mais tarde!");
+            }
+
+        } catch (error) {
+            if (error.response?.data.error) {
+                setMsg(error.response?.data.error);
+            } else {
+                setMsg("Erro ao fazer login, tente novamente mais tarde!");
+            }
+
+            console.log(error);
+        }
+
+
+        
     }
 
     return (
@@ -20,14 +57,33 @@ function Login() {
                     <h5 className="mb-4 text-secondary">Acesse sua conta</h5>
 
                     <div className="mt-4">
-                        <input type="email" placeholder="E-mail" className="form-control" />
+                        <input
+                            type="email"
+                            placeholder="E-mail"
+                            className="form-control"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="mt-2">
-                        <input type="password" placeholder="Senha" className="form-control" />
+                        <input
+                            type="password"
+                            placeholder="Senha"
+                            className="form-control"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                     <div className="mt-3 mb-5">
                         <button onClick={ExecuteLogin} className="btn btn-primary w-100" type="button">Login</button>
                     </div>
+
+                    {
+                        msg.length > 0 &&
+                        <div className="alert alert-danger" role="alert">
+                            {msg}
+                        </div>
+                    }
+
+
 
                     <div>
                         <span className="me-1">Não tenho uma conta.</span>
